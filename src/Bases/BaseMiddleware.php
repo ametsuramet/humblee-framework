7c44db00c432960b6c432aed15683f4b0b008a7d
amet suramet <ametsuramet@gmail.com>
2017-08-23 10:59:30 +0700

    delete basemiddleware

--- src/Bases/BaseMiddleware.php
+++ src/Bases/BaseMiddleware.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Middlewares;
+namespace Amet\Humblee\Bases;
 
 class BaseMiddleware {
 
--- src/Bases/BaseModel.php
+++ src/Bases/BaseModel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Amet\Humblee\Bases;
 
 class BaseModel
 {
