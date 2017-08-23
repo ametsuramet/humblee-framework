1c7a19ba16d71c52afae922381c10232d00223a6
amet suramet <ametsuramet@gmail.com>
2017-08-23 10:33:06 +0700

    first init

---
+++ Providers/RouterProviders/RouteCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Amet\Humblee\Providers\RouteProviders;
+
+class RouteCollection extends \SplObjectStorage
+{
+   
+    public function attachRoute(Route $attachObject)
+    {
+        parent::attach($attachObject, null);
+    }
+
+    public function all()
+    {
+        $temp = array();
+        foreach ($this as $route) {
+            $temp[] = $route;
+        }
+
+        return $temp;
+    }
+}
